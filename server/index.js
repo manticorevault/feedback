fff3a9e375aa017aedb5a0cf1c28e9d41e3b1509
manticorevault <artur_seilah@hotmail.com>
2021-07-05 11:05:41 -0300

    Connected to the express server and startedthe application on localhost:3333

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
 const express = require("express");
+
+// Bring in the express app
+const app = express();
+
+// Get request on the home page
+app.get("/", (req, res) => {
+    res.send({ test: "line!" });
+});
+
+// Log the port where it's running
+app.listen(3333, () => {
+    console.log("App running on port 3333")
+});
